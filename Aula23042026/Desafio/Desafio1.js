25682dd061a1032f8bbfb9d8dd2f641d749bff3d
Th1ago-V <thiagomacielmoura2009@gmail.com>
2026-04-27 17:19:26 -0300

    Limit guest list to 50 and notify when full

--- Aula23042026/Desafio/Desafio1.js
+++ Aula23042026/Desafio/Desafio1.js
@@ -2,29 +2,32 @@ let lista = [];
 let controle = 1;
 let i = 0;
 
-while (i < 51){
+while (i <= 50){
     for(j = 0; j<i; ++j){
         console.log(lista[j])
     }
 
     console.log("Digite 1 para adcionar um convidado a lista e digite \"SAIR\" para remover um convidado da lista: ");
 
-if(controle == 1){ // adcionar convidado
-    lista.push("Convidado " + (i+1));
-}
+    if(controle == 1){ // adcionar convidado
+        lista.push("Convidado " + (i+1));
+    }
 
-else if(controle == "SAIR"){ // remover convidado especifico
-    console.log("Entre com o número do convidado que deseja sair: ");
-    let del = 0; //como não há interface para entrada de dados, deixarei aqui como 0. 
-    let index = lista.indexOf("Convidado " + del);
-if (index > -1) {
-  lista.splice(index, 1);
-}
-}
+    else if(controle == "SAIR"){ // remover convidado especifico
+        console.log("Entre com o número do convidado que deseja sair: ");
+        let del = 0; //como não há interface para entrada de dados, deixarei aqui como 0. 
+        let index = lista.indexOf("Convidado " + del);
+    if (index > -1) {
+    lista.splice(index, 1);
+    }
+    }
 
-else{ // caso digitada algo diferente de "1" ou "SAIR"
-    console.log("Opção inválida")
-}
+    else{ // caso digitada algo diferente de "1" ou "SAIR"
+        console.log("Opção inválida")
+    }
 
-i = lista.length
+    i = lista.length
+    if(i == 51){
+        console.log("Lotação esgotada! Temos 50 inscritos.")
+    }
 }
